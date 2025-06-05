7385bf3da43a4f424994f6220d0f653fee416677
Guilherme <guijhonyt@gmail.com>
2025-06-05 20:34:26 -0300

    criando classe pedido

---
+++ P2_POO/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_POO
+{
+    class Pedido
+    {
+        public Guid Id { get; private set; }
+        public Cliente Cliente { get; private set; }
+        public List<Produto> Produtos { get; private set; }
+        public DateTime Data { get; private set; }
+        public decimal ValorTotal => Produtos.Sum(p => p.Preco);
+
+        public Pedido(Cliente cliente, List<Produto> produtos)
+        {
+            if (cliente == null)
+                throw new ArgumentException(nameof(cliente));
+
+            if (produtos == null || produtos.Count <= 0)
+                throw new ArgumentException("O pedido deve ter pelo menos 1 produto");
+
+            Id = Guid.NewGuid();
+            Cliente = cliente;
+            Produtos = new List<Produto>(produtos);
+            Data = DateTime.Now;
+        }
+    }
+}
