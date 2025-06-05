0f2e3e19306ce8db48c4bcfe296e1a890c1e5754
Guilherme <guijhonyt@gmail.com>
2025-06-05 18:27:53 -0300

    criando classe Produto

---
+++ P2_POO/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_POO
+{
+    public class Produto
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+        public string Categoria { get; private set; }
+
+        public Produto(string nome, decimal preco, string categoria)
+        {
+          
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome do produto invalido.");
+
+            if (preco <= 0)
+                throw new ArgumentException("Preço do produto invalido.");
+
+            if (string.IsNullOrEmpty(categoria))
+                throw new ArgumentException("Categoria do produto invalida");
+
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+            Id = Guid.NewGuid();
+        }
+    }
+}
