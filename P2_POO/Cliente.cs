3a49733b3879abbfee9489c3b5778f4edd73d558
Guilherme <guijhonyt@gmail.com>
2025-06-05 18:59:52 -0300

    criando classe Cliente

---
+++ P2_POO/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_POO
+{
+    class Cliente
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+        public string CPF { get; private set; }
+
+
+        public Cliente(string nome, string email, string cpf)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome do cliente invalido");
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentException("E-mail do cliente invalido");
+
+            if (string.IsNullOrEmpty(cpf))
+                throw new ArgumentException("CPF do cliente ivalido");
+
+            Nome = nome;
+            Email = email;
+            CPF = cpf;
+            Id = Guid.NewGuid();
+            
+        }
+    }
+}
