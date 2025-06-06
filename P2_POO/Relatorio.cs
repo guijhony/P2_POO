206025deb828f3dbf4dea4312a44256872d6204c
Guilherme <guijhonyt@gmail.com>
2025-06-05 21:27:20 -0300

    criando classe Relatorio

---
+++ P2_POO/Relatorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_POO
+{
+    class Relatorio
+    {
+        public void GerarRelatorio(List<Pedido> pedidos)
+        {
+
+            foreach (var pedido in pedidos)
+            {
+                Console.WriteLine($"Pedido ID: {pedido.Id}");
+                Console.WriteLine($"Cliente: {pedido.Cliente.Nome}");
+                Console.WriteLine($"Data: {pedido.Data}");
+
+                foreach (var produto in pedido.Produtos)
+                {
+                    Console.WriteLine("Produtos:");
+
+                    Console.WriteLine($" {produto.Nome} - Preço: {produto.Preco:C} - Categoria: {produto.Categoria}");
+                }
+
+                Console.WriteLine($"Valor Total: {pedido.ValorTotal:C}");
+            }
+        }
+    }
+}
