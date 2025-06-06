acca23a2ea8470f53d522dda99d1690a0bd6b832
Guilherme <guijhonyt@gmail.com>
2025-06-06 14:48:07 -0300

    testando se cadastro de cliente, produto, pedido, relatorio e tratação de exceção estao funcinando

--- P2_POO/Program.cs
+++ P2_POO/Program.cs
@@ -6,16 +6,18 @@ class Program
     static void Main(string[] args)
     {
         Cliente cliente1 = new Cliente("Guilherme", "gui@gmail.com", "000000000/00");
+        //testando se ia dar erro declarando dados ivalidos
+        //Cliente cliente2 = new Cliente("", "", "");
 
         Produto produto1 = new Produto("Notebook", 3000.00m, "Eletronico");
         Produto produto2 = new Produto("Geladeira", 5000m, "Eletrodomssticos");
+
         Produto produto3 = new Produto("Boneca", 100m, "Brinquedo");
         
         Pedido pedido1 = new Pedido(cliente1, new List<Produto> { produto1, produto2 });
 
-        var pedidos = new List<Pedido> { pedido };
+        var pedidos = new List<Pedido> { pedido1 };
 
-        
         var relatorio = new Relatorio();
         relatorio.GerarRelatorio(pedidos);
     }
